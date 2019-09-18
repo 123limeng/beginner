0e3d03a15b719e3602953c739e6927cb41dced37
limeng <limengd@mail.taiji.com.cn>
2019-09-18 17:59:23 +0800

    自定义穿梭框

--- src/components/Element_ui/components/transfer/Customize_Transfer.vue
+++ src/components/Element_ui/components/transfer/Customize_Transfer.vue
@@ -1,10 +1,47 @@
 <template>
   <div>
     <h3>使用render-content自定义数据项</h3>
+    <el-transfer
+      :titles="['左边', '右边']"
+      :button-texts="['往左边', '往右边']"
+      :render-content="renderFunc"
+      v-model="value"
+      :data="data"
+      :format="{
+        noChecked: '${total}',
+        hasChecked: '${checked}/${total}'
+      }"
+      />
+    <h3>使用scoped-slot自定义数据项</h3>
+    <el-transfer
+      :data="data"
+      v-model="value"
+      :titles="['left', 'right']"
+      :button-texts="['toLeft', 'toRight']">
+      <span slot-scope="{ option }">==>{{ option.label }}</span>
+    </el-transfer>
   </div>
 </template>
 <script>
 export default {
-  name: 'CustomizeTransfer'
+  name: 'CustomizeTransfer',
+  data () {
+    return {
+      value: [],
+      data: [
+        {
+          key: 0,
+          label: '包子'
+        },
+        {
+          key: 1,
+          label: '饺子'
+        }
+      ],
+      renderFunc (h, option) {
+        return <span>>> { option.label }</span>
+      }
+    }
+  }
 }
 </script>
