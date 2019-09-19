d58898a2cd51e066207c84251d3412d26f2ba1be
limeng <limengd@mail.taiji.com.cn>
2019-09-19 09:50:00 +0800

    穿梭框数据项属性别名

---
+++ src/components/Element_ui/components/transfer/Alias_Transfer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h3>数据项属性别名</h3>
+    <el-transfer
+      :data="data"
+      v-model="value"
+      :props="{
+        key: 'value',
+        label: 'desc'
+      }"/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AliasTransfer',
+  data () {
+    return {
+      value: [],
+      data: [
+        {
+          value: '0',
+          desc: '太极'
+        },
+        {
+          value: '1',
+          desc: '美团'
+        },
+        {
+          value: '2',
+          desc: '阿里巴巴'
+        }
+      ]
+    }
+  }
+}
+</script>
