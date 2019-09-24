2dd223561c98047e3f0acb4f1cb9df3d073a304a
limeng <limengd@mail.taiji.com.cn>
2019-09-24 16:16:00 +0800

    组件通讯

---
+++ src/components/Vue/components/Basics/Child_test_component.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h4>
+      \(^o^)/~用来测试的子组件
+    </h4>
+    <span>{{`这是从我爸爸那继承来的口头禅————${msg}`}}</span>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ChildTestComponent',
+  props: [
+    'msg'
+  ],
+  data () {
+    return {
+      childData: '我是儿子'
+    }
+  },
+  created () {
+    this.fn()
+  },
+  methods: {
+    fn () {
+      this.$emit('pfn', this.childData)
+    }
+  }
+}
+</script>
